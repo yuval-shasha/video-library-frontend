434f896d8b497f17208ef5f138727f9ce6ecc04f
yuval-shasha <yuvalshasha11@gmail.com>
2025-12-22 17:23:43 +0200

    adding links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import "./App.css";
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 function App() {
 	return (
-		<main className="container">
-			<Movies />
-		</main>
+		<React.Fragment>
+			<NavBar />
+			<main className="container">
+				<Routes>
+					<Route path="/movies/:id" element={<MovieForm />} />
+					<Route path="/movies" element={<Movies />} />
+					<Route path="/customers" element={<Customers />} />
+					<Route path="/rentals" element={<Rentals />} />
+					<Route path="/not-found" element={<NotFound />} />
+					<Route path="/" element={<Navigate to="/movies" replace />} />
+					<Route path="*" element={<Navigate to="/not-found" replace />} />
+				</Routes>
+			</main>
+		</React.Fragment>
 	);
 }
 
